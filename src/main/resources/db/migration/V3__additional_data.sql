a42ed8f1c7d6eb73112cd21a2e802810ede1b8e5
Nikolay <157130401678@mail.ru>
2021-12-03 21:09:57 +0300

    init

---
+++ src/main/resources/db/migration/V3__additional_data.sql
@@ -0,0 +1,50 @@
+create unique index products_id_uindex
+    on products (id);
+
+create unique index products_title_uindex
+    on products (title);
+
+INSERT INTO products (title, cost)
+VALUES ('Meat', 800);
+
+INSERT INTO products (title, cost)
+VALUES ('Avocado', 420);
+
+INSERT INTO products (title, cost)
+VALUES ('Bread', 50);
+
+INSERT INTO products (title, cost)
+VALUES ('Chicken', 540);
+
+INSERT INTO products (title, cost)
+VALUES ('Seafood', 900);
+
+INSERT INTO products (title, cost)
+VALUES ('Ice cream', 80);
+
+INSERT INTO products (title, cost)
+VALUES ('Sauces', 140);
+
+INSERT INTO products (title, cost)
+VALUES ('Milk', 120);
+
+INSERT INTO products (title, cost)
+VALUES ('Fish', 135);
+
+INSERT INTO products (title, cost)
+VALUES ('Tomato', 400);
+
+INSERT INTO products (title, cost)
+VALUES ('Melon', 500);
+
+INSERT INTO products (title, cost)
+VALUES ('Mozzarella', 560);
+
+INSERT INTO products (title, cost)
+VALUES ('Yogurt', 100);
+
+INSERT INTO products (title, cost)
+VALUES ('Butter', 500);
+
+INSERT INTO products (title, cost)
+VALUES ('Blueberry', 900);
